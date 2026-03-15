a09d8ab5471d3a73591f77b9acc0cc228bedfbad
뭘보노 <95015951+Aiden0547@users.noreply.github.com>
2026-03-16 08:50:12 +0900

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("simCanvas")
 const ctx = canvas.getContext("2d")
 
-canvas.width = 900
-canvas.height = 600
+canvas.width = 1000
+canvas.height = 700
 
 const k = 8.99e9
 const EPS = 25
